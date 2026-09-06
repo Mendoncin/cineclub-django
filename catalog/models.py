d5560b7c8585e0028b087a16dca94f4af9d43511
Gabriel Alencar <gabriel.cabecao.lol@gmail.com>
2026-09-06 17:24:07 -0300

    feat: Add genre model

--- catalog/models.py
+++ catalog/models.py
@@ -5,3 +5,13 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a movie genre (e.g. Action, Comedy, Drama)")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
