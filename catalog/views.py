cac6c7c5ec4cd3da4b34be22709731d20199f640
Gabriel Alencar <gabriel.cabecao.lol@gmail.com>
2026-09-06 21:46:40 -0300

    feat: Add homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from catalog.models import Movie, Genre, Director
 
 # Create your views here.
+
+
+def index(request):
+    num_movies = Movie.objects.count()
+    num_directors = Director.objects.count()
+    num_genres = Genre.objects.count()
+
+    context = {
+        "num_movies": num_movies,
+        "num_directors": num_directors,
+        "num_genres": num_genres,
+    }
+    return render(request, "catalog/home.html", context)
